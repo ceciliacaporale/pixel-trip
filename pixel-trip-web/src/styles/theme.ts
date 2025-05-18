b8e5a448e760ec15a508845eeb8d7864d09638c2
ceciliacaporale <cecicaporale16@gmail.com>
2025-05-17 21:43:09 -0300

    feat: criando tokens do tema

---
+++ pixel-trip-web/src/styles/theme.ts
@@ -0,0 +1,34 @@
+
+import type { DefaultTheme } from 'styled-components';
+
+export const lightTheme: DefaultTheme = {
+    colors: {
+        lightOlive: '#C5D9A8',
+        sand: '#FAD5AF',
+        cream: '#FEFBE5',
+        leafGreen: '#87C152',
+        forestGreen: '#549357',
+        terraCotta: '#8C593A',
+        deepClay: '#612238',
+        parchment: '#CDCBA0',
+        nearlyBlack: '#000100',
+        mistyBlue: '#9FA6B1',
+        darkMode: {} as any,
+    }
+};
+
+export const darkTheme: DefaultTheme = {
+  colors: {
+    lightOlive: '#4B5A2B', 
+    sand: '#7B5E3C',      
+    cream: '#2B2A23',        
+    leafGreen: '#569243', 
+    forestGreen: '#2F4B2F', 
+    terraCotta: '#5B3A29',
+    deepClay: '#3B1926',  
+    parchment: '#6D6B50',  
+    nearlyBlack: '#E5E5E5', 
+    mistyBlue: '#C1C7D0',
+    darkMode: {} as any,
+  },
+};
